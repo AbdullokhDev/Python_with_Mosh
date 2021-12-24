fdb60ef7bfc9431280382205a3c1d3e99b69a862
Abdullokh <91453062+AbdullokhDev@users.noreply.github.com>
2021-12-24 08:38:03 +0500

    2

--- Data structures.py
+++ Data structures.py
@@ -28,19 +28,60 @@
 # for letter in enumerate(letters):
 #     print(letter)
 
-# ADD and REMOVE
-letters = ['a','b','c','d']
-# append funct is adding new object at the end of list while
-# insert you should specify number of index
-letters.append('e')
-letters.insert(0, '-')
+# # ADD and REMOVE
+# letters = ['a','b','c','d']
+# # append funct is adding new object at the end of list while
+# # insert you should specify number of index
+# letters.append('e')
+# letters.insert(0, '-')
 
-# pop func is remove by index while
-# remove function is removes only by itself
-# delete func is removes range of objects while
-# clear func is removes all of them
-letters.pop(0)
-letters.remove("b")
-del letters[0:3]
-letters.clear()
-print(letters)
+# # pop func is remove by index while
+# # remove function is removes only by itself
+# # delete func is removes range of objects while
+# # clear func is removes all of them
+# letters.pop(0)
+# letters.remove("b")
+# del letters[0:3]
+# letters.clear()
+# print(letters)
+
+# LAMBDA FUNCTION
+# items = [
+#     ('Product1', 10),
+#     ('Prroduct2', 9),
+#     ('Product3', 12)
+#    ]
+
+# items.sort(key=lambda item: item[1])
+# print(items)
+
+# items = [
+#     ('Product1', 10),
+#     ('Prroduct2', 9),
+#     ('Product3', 12)
+#     ]
+# prices = []
+# for item in items:
+#     prices.append(item[1])
+    
+# print(prices)
+
+items = [
+    ("Product1", 10),
+    ("Prroduct2", 9),
+    ("Product3", 12),
+    ]
+x = map(lambda item: item[1], items)
+for item in x:
+    print(item)
+
+
+
+
+
+
+
+
+
+
+
