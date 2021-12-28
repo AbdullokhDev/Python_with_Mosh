818d222c57c3014a6b310080511c42dfd605c0aa
Abdullokh <91453062+AbdullokhDev@users.noreply.github.com>
2021-12-28 10:55:54 +0500

    updated classes.py

--- Classes.py
+++ Classes.py
@@ -4,6 +4,10 @@
 # Class: Human
 # Objects: Abdulloh, Muhammad, Abu Bakr, Umar, Usmon, Ali
 
+# Class: Car
+# Instance: 2017 Honda Civic
+# Attributes: weight, color would be an attribute of that instance of that class
+
 # class Point:
 #     def draw(self):
 #         print("draw")
@@ -13,13 +17,75 @@
 # print(isinstance(point, Point))
 
 # CONSTRUCTOR
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+        
+#     def __str__(self):
+#         return f"({self.x}, {self.y})"
+    
+#     def draw(self):
+#         print(f"Point ({self.x}, {self.y})")
+        
+# point = Point(1, 2)
+# print(str(point))
+# point.draw()
+
+# another = Point(3, 4)
+# another.draw()
+
+# All Humans have two eyes, two ears, so these are attributes we'll define
+# at the class level, and they are shared among all intances
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
-    def draw(self):
-        print(f"Point ({self.x}, {self.y})")
         
+    def __gt__(self, other):
+        return self.x == other.x and self.y == other.y
+
 point = Point(1, 2)
-point.draw()
+other = Point(1, 2)
+print(point > other)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
